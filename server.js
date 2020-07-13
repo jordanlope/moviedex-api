f555a12a70394846ab7cf24351ec9ea1b67e11b7
Jordan Lopez <jordanlopez992@gmail.com>
2020-07-13 13:30:36 -0400

    Fix unauthorized request status code

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization')
 
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(400).json({ error: 'Unauthorization for this user' })
+        return res.status(401).json({ error: 'Unauthorization user' })
     }
 
     next()
